e3f0ac281ba1e01f72ecbc19e16dbfdcad42890e
ThatMadMf <maxdarksiders@gmail.com>
2020-11-17 02:26:26 +0200

    Implement jwt authentication middleware

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+const authentication = require('../services/authentication');
 const controller = require('../controllers/users');
 
-router.get('/', controller.getUsers);
+router.get('/', authentication, controller.getUsers);
 router.post('/register', controller.register);
 router.post('/login', controller.login)
 
