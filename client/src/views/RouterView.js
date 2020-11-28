9669e36eebe24f26c8a56feff1964b6bf17ea1bb
ThatMadMf <maxdarksiders@gmail.com>
2020-11-28 11:38:15 +0200

    Justify menu items

--- client/src/views/RouterView.js
+++ client/src/views/RouterView.js
@@ -2,17 +2,23 @@ import React from 'react';
 import {BrowserRouter as Router, Link, Switch} from "react-router-dom";
 import {Menu} from "antd";
 
+const menuStyle = {
+    position: "relative",
+    display: "flex",
+    justifyContent: "space-between"
+}
+
 export const RouterView = () =>
     <Router>
-        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+        <Menu theme="light" mode="horizontal" defaultSelectedKeys={['2']} style={menuStyle}>
             <Menu.Item key="1">
-                <Link to="/">nav 1</Link>
+                <Link to="/blog">Blog</Link>
             </Menu.Item>
-            <Menu.Item key="2">
-                <Link to="/">nav 2</Link>
+            <Menu.Item key="2" disabled={true}>
+                <Link to="/gallery">Gallery</Link>
             </Menu.Item>
-            <Menu.Item key="3">
-                <Link to="/">nav 3</Link>
+            <Menu.Item key="3" disabled={true}>
+                <Link to="/info">Info</Link>
             </Menu.Item>
         </Menu>
         <Switch>
