c7d90c567c3c20b124e66cef82def2318a48b353
ThatMadMf <maxdarksiders@gmail.com>
2020-12-20 17:17:10 +0200

    Implement photos controller

--- server/src/routes/collections.js
+++ server/src/routes/collections.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-const multer = require('multer');
-const storage = multer.memoryStorage();
-
 const controller = require('../controllers/collections');
 
 router.get('/', controller.getCollections);
