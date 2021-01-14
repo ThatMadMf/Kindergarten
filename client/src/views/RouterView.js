94fb642d0c92b461f0bfbe83a62a71048080ac2f
ThatMadMf <maxdarksiders@gmail.com>
2021-01-14 22:10:24 +0200

    Implement login page

--- client/src/views/RouterView.js
+++ client/src/views/RouterView.js
@@ -7,6 +7,7 @@ import InfoView from "./InfoView";
 import {Redirect} from "react-router";
 import {CollectionView} from "./CollectionView";
 import {LoginView} from "./LoginView";
+import {useSelector} from "react-redux";
 
 const menuStyle = {
     position: "relative",
@@ -28,45 +29,51 @@ const registerButton = {
     height: '100%',
 }
 
-export const RouterView = () =>
-    <Router>
-        <Menu theme="light" mode="horizontal" style={menuStyle}>
-            <Menu.Item key="1">
-                <Link to="/blog">Blog</Link>
-            </Menu.Item>
-            <Menu.Item key="2">
-                <Link to="/gallery">Gallery</Link>
-            </Menu.Item>
-            <Menu.Item key="3">
-                <Link to="/info">Info</Link>
-            </Menu.Item>
-            <button style={loginButton}>
-                <Link to="/login">Login</Link>
-            </button>
-            <button style={registerButton}>
-                <Link to="/register">Register</Link>
-            </button>
-        </Menu>
-        <Switch>
-            <div className="switch-wrapper">
-                <Route exact path={'/'}>
-                    <Redirect to={'/blog'}/>
-                </Route>
-                <Route exact path={'/blog'}>
-                    <BlogView/>
-                </Route>
-                <Route exact path={'/gallery'}>
-                    <GalleryView/>
-                </Route>
-                <Route exact path={'/collections/:collectionId'}>
-                    <CollectionView/>
-                </Route>
-                <Route exact path={'/info'}>
-                    <InfoView/>
-                </Route>
-                <Route exact path={'/login'}>
-                    <LoginView/>
-                </Route>
-            </div>
-        </Switch>
-    </Router>
+export const RouterView = () => {
+    const user = useSelector((store) => store.userModule.authorizedUser);
+
+    return (
+        <Router>
+            <Menu theme="light" mode="horizontal" style={menuStyle}>
+                <Menu.Item key="1">
+                    <Link to="/blog">Blog</Link>
+                </Menu.Item>
+                <Menu.Item key="2">
+                    <Link to="/gallery">Gallery</Link>
+                </Menu.Item>
+                <Menu.Item key="3">
+                    <Link to="/info">Info</Link>
+                </Menu.Item>
+                {user == null && <button style={loginButton}><Link to="/login">Login</Link></button>}
+                {user == null && <button style={registerButton}><Link to="/register">Register</Link></button>}
+                {user && <p>{user.username}</p>}
+            </Menu>
+            <Switch>
+                <div className="switch-wrapper">
+                    <Route exact path={'/'}>
+                        <Redirect to={'/blog'}/>
+                    </Route>
+                    <Route exact path={'/blog'}>
+                        <BlogView/>
+                    </Route>
+                    <Route exact path={'/gallery'}>
+                        <GalleryView/>
+                    </Route>
+                    <Route exact path={'/collections/:collectionId'}>
+                        <CollectionView/>
+                    </Route>
+                    <Route exact path={'/info'}>
+                        <InfoView/>
+                    </Route>
+                    <Route exact path={'/login'}>
+                        <LoginView/>
+                    </Route>
+                    <Route exact path={'/register'}>
+
+                    </Route>
+                </div>
+            </Switch>
+        </Router>
+    )
+}
+
