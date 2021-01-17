2ddda306bc016f7d87ed75a2bf6dd5c620330bd5
ThatMadMf <maxdarksiders@gmail.com>
2021-01-17 17:13:00 +0200

    Implement authorization headers util

--- client/src/services/ApiService.js
+++ client/src/services/ApiService.js
@@ -2,11 +2,19 @@ import Axios from 'axios';
 
 const api = 'http://localhost:8000/api/';
 
+export const getAuthHeaders = () => {
+    return {
+        headers: {
+            Authorization: localStorage.getItem('token'),
+        },
+    };
+}
+
 export const ApiService = {
     get(path) {
-        return Axios.get(api + path);
+        return Axios.get(api + path, getAuthHeaders());
     },
     post(path, data) {
-        return Axios.post(api + path, data);
+        return Axios.post(api + path, data, getAuthHeaders());
     },
 }
