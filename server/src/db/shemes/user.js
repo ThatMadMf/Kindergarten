071499dbe48119456f3a2cafea852ba4e91de818
ThatMadMf <maxdarksiders@gmail.com>
2021-01-18 18:17:28 +0200

    Alter user scheme with new fields

--- server/src/db/shemes/user.js
+++ server/src/db/shemes/user.js
@@ -2,19 +2,28 @@ const mongoose = require('mongoose');
 
 mongoose.set('useCreateIndex', true);
 
-const userScheme = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    },
-},
+const userScheme = new mongoose.Schema(
     {
-        timestamps: { createdAt: 'createdAt' }
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        firstName: {
+            type: String,
+            required: false,
+        },
+        lastName: {
+            type: String,
+            required: false,
+        },
+    },
+    {
+        timestamps: {createdAt: 'createdAt'}
     }
 );
 
